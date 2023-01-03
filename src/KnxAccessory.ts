c537b63cf8bce3988cf3a729f4105de4fc924f28
marcinm <mm.kodmax@gmail.com>
2023-01-03 15:17:59 +0100

    refactor - added KnxAccessory

---
+++ src/KnxAccessory.ts
@@ -0,0 +1,68 @@
+import { API, Logging } from 'homebridge'
+import { KnxLink } from 'js-knx'
+
+import { KnxPlatformAccessory } from './KnxPlatformAccessory'
+import { KnxAccessoryConfig } from './config'
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings'
+
+import { CarbonDioxiditeSensor } from './service/CarbonDioxiditeSensor'
+import { Lightbulb } from './service/Lightbulb'
+import { AbstractKnxService } from './service/AbstractKnxService'
+
+class KnxAccessory {
+    public readonly displayName: string
+    public readonly uuid: string
+
+    private services: AbstractKnxService[] = []
+
+    private getAccessoryUUID (config: KnxAccessoryConfig): string {
+        const addresses = config.services.map(service => service.addresses.join(',')).join(',')
+        return this.api.hap.uuid.generate(`${PLATFORM_NAME}.${this.config.name}.${addresses}`)
+    }
+
+    private getAccessoryDisplayName (config: KnxAccessoryConfig): string {
+        return `${config.name} ${config.services[0].name}`
+    }
+
+    public constructor (
+        private config: KnxAccessoryConfig,
+        private logger: Logging,
+        private knx: KnxLink,
+        private api: API
+    ) {
+        this.displayName = this.getAccessoryDisplayName(config)
+        this.uuid = this.getAccessoryUUID(config)
+    }
+
+    public register (): KnxPlatformAccessory {
+        const displayName = this.getAccessoryDisplayName(this.config)
+        this.logger.debug('registering knx accessory', displayName)
+        // eslint-disable-next-line new-cap
+        const accessory = new this.api.platformAccessory(displayName, this.uuid) as KnxPlatformAccessory
+        accessory.context = this.config
+
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory])
+        return accessory
+    }
+
+    public setupServices (accessory: KnxPlatformAccessory): void {
+        for (const service of accessory.context.services) {
+            switch (service.id) {
+
+                case 'Lightbulb':
+                    this.services.push(new Lightbulb(this.api, this.knx, accessory, service))
+                    break
+
+                case 'CarbonDioxideSensor':
+                    this.services.push(new CarbonDioxiditeSensor(this.api, this.knx, accessory, service))
+                    break
+
+                default:
+                    throw new Error(`<${service.id}> service not supported`)
+            }
+
+        }
+    }
+}
+
+export { KnxAccessory }
--- src/KnxPlatform.ts
+++ src/KnxPlatform.ts
@@ -2,16 +2,14 @@ import { API, APIEvent, DynamicPlatformPlugin, Logging, PlatformAccessory, Platf
 import { KnxLink } from 'js-knx'
 
 import { KnxPlatformAccessory, isKnxPlatformConfig } from './KnxPlatformAccessory'
-import { KnxAccessoryConfig, KnxPlatformConfig } from './config'
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings'
+import { KnxAccessory } from './KnxAccessory'
 
-import { CarbonDioxiditeSensor } from './service/CarbonDioxiditeSensor'
-import { Lightbulb } from './service/Lightbulb'
-import { AbstractKnxService } from './service/AbstractKnxService'
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings'
+import { KnxPlatformConfig } from './config'
 
 class KnxPlatform implements DynamicPlatformPlugin {
-    private accessories: Array<KnxPlatformAccessory> = []
-    private services: AbstractKnxService[] = []
+    private knxAccessories: Map<string, KnxAccessory> = new Map()
+    private hbAccessories: Array<KnxPlatformAccessory> = []
     private config: KnxPlatformConfig
 
     private async connect (): Promise<KnxLink> {
@@ -39,74 +37,37 @@ class KnxPlatform implements DynamicPlatformPlugin {
     }
 
     public configureAccessory (accessory: PlatformAccessory<UnknownContext>): void {
-        this.accessories.push(accessory as KnxPlatformAccessory)
-    }
-
-    private getAccessoryUUID (config: KnxAccessoryConfig): string {
-        const addresses = config.services.map(service => service.addresses.join(',')).join(',')
-        return this.api.hap.uuid.generate(`${PLATFORM_NAME}.${this.config.name}.${addresses}`)
-    }
-
-    private getAccessoryDisplayName (config: KnxAccessoryConfig): string {
-        return `${config.name} ${config.services[0].name}`
+        this.hbAccessories.push(accessory as KnxPlatformAccessory)
     }
 
     private configureAccessories (knx: KnxLink): void {
-        const alreadyRegistered = new Set(this.accessories.map(acc => acc.UUID))
-        for (const config of this.config.accessories) {
-            const uuid = this.getAccessoryUUID(config)
+        const alreadyRegistered = new Set(this.hbAccessories.map(acc => acc.UUID))
 
-            if (!alreadyRegistered.has(uuid)) {
-                this.registerAccessory(uuid, config)
+        for (const config of this.config.accessories) {
+            const knxAccessory = new KnxAccessory(config, this.logger, knx, this.api)
+            if (!alreadyRegistered.has(knxAccessory.uuid)) {
+                knxAccessory.register()
             }
+
+            this.knxAccessories.set(knxAccessory.uuid, knxAccessory)
         }
 
-        const isConfigured = new Set(this.config.accessories.map(config => this.getAccessoryUUID(config)))
-        for (const accessory of this.accessories) {
-            if (isConfigured.has(accessory.UUID)) {
+        for (const accessory of this.hbAccessories) {
+            const configuredAccessory = this.knxAccessories.get(accessory.UUID)
+            if (configuredAccessory) {
                 try {
-                    this.setupAccessory(knx, accessory)
+                    configuredAccessory.setupServices(accessory)
 
                 } catch (e) {
                     this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory])
+                    this.logger.debug('unregistered knx accessory', accessory.displayName)
                     throw e
                 }
 
             } else {
-                this.logger.debug('unregistering knx accessory', accessory.displayName)
                 this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory])
+                this.logger.debug('unregistered knx accessory', accessory.displayName)
             }
-        }
-    }
-
-    private registerAccessory (uuid: string, config: KnxAccessoryConfig): KnxPlatformAccessory {
-        const displayName = this.getAccessoryDisplayName(config)
-        this.logger.debug('registering knx accessory', displayName)
-        // eslint-disable-next-line new-cap
-        const accessory = new this.api.platformAccessory(displayName, uuid) as KnxPlatformAccessory
-        accessory.context = config
-
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory])
-        this.accessories.push(accessory)
-        return accessory
-    }
-
-    private setupAccessory (knx: KnxLink, accessory: KnxPlatformAccessory): void {
-        for (const service of accessory.context.services) {
-            switch (service.id) {
-
-                case 'Lightbulb':
-                    this.services.push(new Lightbulb(this.api, knx, accessory, service))
-                    break
-
-                case 'CarbonDioxideSensor':
-                    this.services.push(new CarbonDioxiditeSensor(this.api, knx, accessory, service))
-                    break
-
-                default:
-                    throw new Error(`<${service.id}> service not supported`)
-            }
-
         }
     }
 }
