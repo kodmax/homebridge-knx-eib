b45fcf43988bcf563e8554e20284692cedcefba4
marcinm <mm.kodmax@gmail.com>
2023-01-04 23:04:01 +0100

    added heating and cooling state for thermostat

--- src/service/Thermostat.ts
+++ src/service/Thermostat.ts
@@ -21,7 +21,7 @@ class Thermostat extends AbstractKnxService {
         addTargetTemperatureCharacteristic(api, service, knx, config.addresses[0], config.addresses[1])
         addCurrentTemperatureCharacteristic(api, service, knx, config.addresses[2])
 
-        addCurrentHeatingCoolingStateCharacteristic(api, service)
+        addCurrentHeatingCoolingStateCharacteristic(api, service, knx, config.addresses[3], config.addresses[4])
         addTargetHeatingCoolingStateCharacteristic(api, service)
         addTemperatureDisplayUnitsCharacteristic(api, service)
     }
--- src/service/characteristic/CurrentHeatingCoolingState.ts
+++ src/service/characteristic/CurrentHeatingCoolingState.ts
@@ -1,11 +1,30 @@
 import { API, Service } from 'homebridge'
+import { DPT_State, KnxLink } from 'js-knx'
 
-const addCurrentHeatingCoolingStateCharacteristic = (api: API, service: Service): void => {
+const addCurrentHeatingCoolingStateCharacteristic = (api: API, service: Service, knx: KnxLink, heatingStateAddress?: string, coolingStateAddress?: string): void => {
     const targetState = service.getCharacteristic(api.hap.Characteristic.CurrentHeatingCoolingState)
+    
+    const heatingState = !!heatingStateAddress && knx.getDatapoint({ address: heatingStateAddress, DataType: DPT_State })
+    const coolingState = !!coolingStateAddress && knx.getDatapoint({ address: coolingStateAddress, DataType: DPT_State })
+    
+    if (heatingState || coolingState) {
+        targetState.onGet(async () => {
+            if (heatingState && (await heatingState.read()).value === DPT_State.ACTIVE) {
+                return api.hap.Characteristic.CurrentHeatingCoolingState.HEAT
 
-    targetState.onGet(async () => {
-        return api.hap.Characteristic.CurrentHeatingCoolingState.OFF
-    })
+            } else if (coolingState && (await coolingState.read()).value === DPT_State.ACTIVE) {
+                return api.hap.Characteristic.CurrentHeatingCoolingState.COOL
+
+            } else {
+                return api.hap.Characteristic.CurrentHeatingCoolingState.OFF
+            }
+        })    
+
+    } else {
+        targetState.onGet(async () => {
+            return api.hap.Characteristic.CurrentHeatingCoolingState.OFF
+        })    
+    }
 }
 
 export { addCurrentHeatingCoolingStateCharacteristic }
